19a2cad55dd906067a6d3c2f0967ea1746e9148a
Higor Menezes <higormenezes1996@gmail.com>
2019-10-06 15:25:58 -0400

    Socket
    Create real time communication between frontend and backend

--- src/pages/ListPage/index.js
+++ src/pages/ListPage/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { AsyncStorage, ScrollView } from "react-native";
+import { AsyncStorage, ScrollView, Alert } from "react-native";
+import socketio from "socket.io-client";
 
 import logo from "../../assets/logo.png";
 
@@ -9,6 +10,22 @@ import SpotList from "../../components/SpotList";
 
 function ListPage() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("user").then(user_id => {
+      const socket = socketio("http://192.168.0.113:3333", {
+        query: { user_id }
+      });
+
+      socket.on("booking_response", booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? "APROVADA" : "REJEITADA"
+          }`
+        );
+      });
+    });
+  }, []);
 
   useEffect(() => {
     AsyncStorage.getItem("techs").then(storageTechs => {
